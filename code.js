1d666b88cdaf05876d2f80e054b3f7e36a5a545a
diwash5 <83746921+diwash5@users.noreply.github.com>
2021-09-28 11:28:23 +0000

    Basically Nothing

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 const myUsernames = [];
 const myTotaldownloads = [];
 const myTotaluploads = [];
-
+const datedata = [ ];
+const uploaddata = [ ];
+const downloaddata = [ ];
 
 function getdata() {
     fetch('https://DeepskyblueAlienatedDaemons.diwash5.repl.co/data')
@@ -19,6 +21,21 @@ function getdata() {
                         }
         );
 };
+
+function datetimechart() {
+  fetch('https://DeepskyblueAlienatedDaemons.diwash5.repl.co/rawdata')
+    .then(response => response.json())
+    .then(data => {
+    Object.entries(data).forEach(entry =>{
+    var [key,value] = entry;
+    if ( value.user !== 'AltaiW' ) {
+    datedata.push(value.createdAt);
+    uploaddata.push(value.upload / 1024);
+    downloaddata.push(value.download / 1024 ); }
+    });
+    chartmeplease()},    
+    )
+}
 
 function fuknhell() {
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -63,7 +80,7 @@ options: {
     },
     plugins: {
         legend: {
-            display: true,
+            display: false,
             labels: {
                 color: 'rgba(232, 232, 232, 1)'
             }
@@ -72,6 +89,30 @@ options: {
 }
 });
 };
+function chartmeplease() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+    labels: datedata,
+    datasets: [{
+        label: 'Download (KB/S)',
+        data: downloaddata ,
+        borderColor: 'red',
+        fill: false,
+        tension: 1,
+        rtl : true
+    },
+    {
+        label: 'Upload (KB/S) ',
+        data: uploaddata,
+        borderColor: 'blue' ,
+        borderWidth: 1,
+        radius: 0
+    }]
+}
+} )
+};
 
 const plugin = {
   id: 'custom_canvas_background_color',
@@ -79,7 +120,7 @@ const plugin = {
     const ctx = chart.canvas.getContext('2d');
     ctx.save();
     ctx.globalCompositeOperation = 'destination-over';
-    ctx.fillStyle = 'lightgreen';
+    ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, chart.width, chart.height);
     ctx.restore();
   }
