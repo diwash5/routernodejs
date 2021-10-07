80261f894414ac22172461ae1c4fdbd65e4fa6f5
Diwash Subedi <83746921+diwash5@users.noreply.github.com>
2021-10-07 22:17:26 +0545

    Update code.js

--- code.js
+++ code.js
@@ -1,98 +1,152 @@
-function getdata() {
-  fetch('https://DeepskyblueAlienatedDaemons.diwash5.repl.co/userusage')
+function barchart() {
+fetch('https://routernodejs-1.diwash5.repl.co/userusage')
     .then(response => response.json())
     .then(data => {
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'bar',
-        plugins: [plugin],
-        data: {
-          labels: data.user,
-          datasets: [{
-            label: 'Download',
-            data: data.totaldownloads,
-            backgroundColor: [
-              'rgba(255, 99, 132, 3)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 5)'
-            ],
-            borderWidth: 1
-          },
-          {
-            label: 'Upload',
-            data: data.totaluploads,
-            backgroundColor: [
-              'rgba(54, 162, 235, 0.2)'
-            ],
-            borderColor: [
-              'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          indexAxis: 'y',
-          scales: {
-            x: {
-              stacked: true,
-              beginAtZero: true
+        const chart = Highcharts.chart('barchart', {
+            chart: {
+            type: 'bar'
             },
-            y: {
-              stacked: true
-            }
-          }
-        }
-      });
-    })
-
+            title: {
+                text: 'Total usage'
+            },
+            xAxis: {
+                categories: data.user ,
+    max:8,
+    min:1,
+    scrollbar: {
+    enabled: true
+                },
+      },
+            yAxis: {
+                title: {
+                    text: 'Data in GB'
+                      },
+                min:1,
+                max:20,
+                scrollbar: {
+                    enabled: true
+                }
+            },
+            plotOptions: {
+                    series: {
+                      stacking: 'normal'
+                      }
+                },
+            series: [{
+                name: 'Total Uploads',
+                data: data.totaluploads
+            }, {
+                name: 'Total Downloads',
+                data: data.totaldownloads }]
+        });
+    });
 }
-
-function timeseries() {
-  fetch('https://DeepskyblueAlienatedDaemons.diwash5.repl.co/timeseries')
+function linechart() {
+fetch('https://routernodejs-1.diwash5.repl.co/timeseries')
     .then(response => response.json())
     .then(data => {
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        plugins: [plugin],
-        data: {
-          labels: data.datedata,
-          datasets: [{
-            label: 'Download',
-            data: data.downloaddata,
-            backgroundColor: [
-              'rgba(255, 99, 132, 3)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 5)'
-            ],
-            borderWidth: 1
-          },
-          {
-            label: 'Upload',
-            data: data.uploaddata,
-            backgroundColor: [
-              'rgba(54, 162, 235, 0.2)'
-            ],
-            borderColor: [
-              'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-          }]
-        }
-      });
-    })
+Highcharts.chart('mi4alinechart', {
 
+    title: {
+        text: 'Time line of Data usage'
+    },
+    subtitle: {
+        text: 'Source: Mi-4A'
+    },
+
+    yAxis: {
+        title: {
+            text: 'KB/S'
+        }
+    },
+    xAxis: {
+        categories: data.mi4adates ,
+        scrollbar: {
+          enabled: true
+                },
+        max:30
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+    series: [{
+        name: 'Download',
+        data: data.mi4adownloads
+    }, {
+        name: 'Upload',
+        data: data.mi4auploads
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+Highcharts.chart('mi4clinechart', {
+
+    title: {
+        text: 'Time line of Data usage'
+    },
+
+    subtitle: {
+        text: 'Source: Mi-4C'
+    },
+
+    yAxis: {
+        title: {
+            text: 'KB/S'
+        }
+    },
+
+    xAxis: {
+        categories: data.mi4cdates ,
+        scrollbar: {
+          enabled: true
+                },
+        max:30
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+    series: [{
+        name: 'Download',
+        data: data.mi4cdownloads
+    }, {
+        name: 'Upload',
+        data: data.mi4cuploads
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+ })
 }
-const plugin = {
-  id: 'custom_canvas_background_color',
-  beforeDraw: (chart) => {
-    const ctx = chart.canvas.getContext('2d');
-    ctx.save();
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, chart.width, chart.height);
-    ctx.restore();
-  }
-};
